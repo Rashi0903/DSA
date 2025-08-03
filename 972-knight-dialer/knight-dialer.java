bd3e3a0cac561670146b46bb442f8b54b3467801
Rashika <rashikanegi319@gmail.com>
2025-08-03 18:12:45 +0530

    Time: 23 ms (84.71%) | Memory: 44.9 MB (39.10%) - LeetSync

--- 972-knight-dialer/knight-dialer.java
+++ 972-knight-dialer/knight-dialer.java
@@ -2,16 +2,16 @@ class Solution {
     public int knightDialer(int n) {
         int mod=1000000007;
 
-        int [][]moves={{4,6},
-                        {6,8},
-                        {9,7},
-                        {4,8},
-                        {0,3,9},
-                        {},
-                        {0,1,7},
-                        {2,6},
-                        {1,3},
-                        {2,4}
+        int [][]moves={{4,6},       //0 pai kaha kaha sai reach kr sktai ahi
+                        {6,8},      //1
+                        {9,7},      //2
+                        {4,8},      //3
+                        {0,3,9},    //4 
+                        {},         //5
+                        {0,1,7},    //6
+                        {2,6},      //7
+                        {1,3},      //8
+                        {2,4}       //9
                             };
 
         long prev[]=new long[10];
@@ -27,13 +27,24 @@ class Solution {
             {
                 for(int neigh:moves[digit])
                 {
+                    //moves k 2d array  mai jaak usk neighbours nikalai ga aur fir unko add krrai ga 
+                    /*
+                    neigh=moves[0] => {4,6}
+                    digit=0
+                    next[0]=next[0]+prev[4];    =>next[0]=1
+                    next[0]=next[0]+prev[6];    =>1+1=2
+                    */
                     next[digit]=(next[digit]+prev[neigh])%mod;
                 }
             }
+
+            //next iteration k liyai next ko update krna hai taaki values loose na ho esliyai prev mai next ka poora array daal diya
             prev=next;
         }   
 
         long sum=0;
+
+        //next ka poora array hum already prev mai daal chuk hai esliyai prev[] mai iterate kiya
         for(long i:prev)
         {
             sum=(sum+i)%mod;
@@ -41,3 +52,4 @@ class Solution {
         return (int)sum;
     }
 }
+//mod qki ans int mai chaheyai  input values long mai ho skti hai 
