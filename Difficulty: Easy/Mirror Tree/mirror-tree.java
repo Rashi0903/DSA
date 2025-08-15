6e02fc6df1adaab1dec6551e304ffbe33b66d108
Rashika <rashikanegi319@gmail.com>
2025-08-15 22:12:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Mirror Tree/mirror-tree.java
@@ -0,0 +1,33 @@
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)
+   {
+        data = item;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    void mirror(Node node) {
+        // code here
+        if(node==null)
+        {
+            return;
+        }
+        
+        Node temp=node.left;
+        node.left=node.right;
+        node.right=temp;
+        
+        mirror(node.left);
+        mirror(node.right);
+        
+        
+    }
+}
+/*
+in place krna hai toh left and right ko swap krdo bss 
+*/
