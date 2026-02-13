cedfa8e5727b417cb3a4a9e1a26b634b787ea25b
Rashika <rashikanegi319@gmail.com>
2026-02-13 20:20:00 +0530

    Time: 4 ms (96.43%) | Memory: 46.6 MB (64.99%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+         int n = s.length();
+        int i = 0;
+
+        for (int j = n - 1; j >= 0; j--) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+            }
+        }
+
+        if (i == n) {
+            return s;
+        }
+
+        String remaining = s.substring(i);
+        StringBuilder sb = new StringBuilder(remaining);
+        sb.reverse();
+        return sb.toString()
+                + shortestPalindrome(s.substring(0, i))
+                + remaining;
+    }
+}
